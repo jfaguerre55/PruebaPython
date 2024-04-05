2af3d9f6dda673d61aade4081d7809643e3d8872
juanaguerre <juanaguerre@jubuntu.myguest.virtualbox.org>
2024-04-05 19:53:54 -0300

    Prueba ubuntu

--- main.py
+++ main.py
@@ -6,11 +6,16 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}\n')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def print_numbers():
+    for i in range(1,11):
+        print(i)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    print_numbers()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
